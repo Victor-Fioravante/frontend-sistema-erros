98957846d797fc466d1c68a2c721cd9b0dbda7df
Victor Eduardo Fioravante Araujo De Oliveira <victor.sup.nuvem@alterdata.matriz>
2025-05-29 13:12:19 -0300

    Ajuste no botão de navegação

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'; 
 import './menuStyle.css';
+import { Button } from '@chakra-ui/react';
 
 const MenuBar: React.FC = () => { 
     return (
@@ -8,28 +9,34 @@ const MenuBar: React.FC = () => {
             <nav className="menu-bar">
                 <ul className="menu-list">
                     <li> 
+                        <Button className='button-menu'>
                         <NavLink
                             to="/cadastrar-sugestao" 
                             className='link-ativo'
                         >
                             Cadastrar Sugestão
                         </NavLink>
+                        </Button>
                     </li>
                     <li>
+                        <Button className='button-menu'>
                         <NavLink
                             to="/sugestao"
                             className='link-ativo'
                         >
                             Consultar Sugestões
                         </NavLink>
+                        </Button>
                     </li>
                     <li>
+                        <Button className='button-menu'>
                         <NavLink
                             to="/avaliacao"
                             className='link-ativo'
                         >
                             Consultar Avaliações
                         </NavLink>
+                        </Button>
                     </li>
                 </ul>
             </nav>
